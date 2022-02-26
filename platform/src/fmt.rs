62bae70b8d09eaa0c12e3c750b4b71267911a6be
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-02-25 22:48:59 -0800

    initial changes to test embassy

---
+++ platform/src/fmt.rs
@@ -0,0 +1,15 @@
+#![macro_use]
+
+use defmt_rtt as _; // global logger
+use panic_probe as _;
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+defmt::timestamp! {"{=u64}", {
+        static COUNT: AtomicUsize = AtomicUsize::new(0);
+        // NOTE(no-CAS) `timestamps` runs with interrupts disabled
+        let n = COUNT.load(Ordering::Relaxed);
+        COUNT.store(n + 1, Ordering::Relaxed);
+        n as u64
+    }
+}
--- platform/src/main.rs
+++ platform/src/main.rs
@@ -1,81 +1,80 @@
 #![no_main]
 #![no_std]
+#![feature(type_alias_impl_trait)]
 
-use cortex_m_rt::entry;
-// use embedded_hal::blocking::delay::DelayMs;
-use microbit::{board::Board, display::blocking::Display, hal::Timer};
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
-// use panic_halt as _;
+use embassy::executor::Spawner;
+use embassy::time::{Duration, Timer};
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::Peripherals;
+
+mod fmt;
 
 mod memory;
 
-#[repr(C)]
-#[derive(Debug, Default)]
-struct Row {
-    a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-}
+// #[repr(C)]
+// #[derive(Debug, Default)]
+// struct Row {
+//     a: u8,
+//     b: u8,
+//     c: u8,
+//     d: u8,
+//     e: u8,
+// }
 
-#[repr(C)]
-#[derive(Debug, Default)]
-struct DisplayData {
-    a: Row,
-    b: Row,
-    c: Row,
-    d: Row,
-    e: Row,
-}
+// #[repr(C)]
+// #[derive(Debug, Default)]
+// struct DisplayData {
+//     a: Row,
+//     b: Row,
+//     c: Row,
+//     d: Row,
+//     e: Row,
+// }
 
-impl DisplayData {
-    fn to_bytes(&self) -> [[u8; 5]; 5] {
-        [
-            [self.a.a, self.a.b, self.a.c, self.a.d, self.a.e],
-            [self.b.a, self.b.b, self.b.c, self.b.d, self.b.e],
-            [self.c.a, self.c.b, self.c.c, self.c.d, self.c.e],
-            [self.d.a, self.d.b, self.d.c, self.d.d, self.d.e],
-            [self.e.a, self.e.b, self.e.c, self.e.d, self.e.e],
-        ]
-    }
-}
+// impl DisplayData {
+//     fn to_bytes(&self) -> [[u8; 5]; 5] {
+//         [
+//             [self.a.a, self.a.b, self.a.c, self.a.d, self.a.e],
+//             [self.b.a, self.b.b, self.b.c, self.b.d, self.b.e],
+//             [self.c.a, self.c.b, self.c.c, self.c.d, self.c.e],
+//             [self.d.a, self.d.b, self.d.c, self.d.d, self.d.e],
+//             [self.e.a, self.e.b, self.e.c, self.e.d, self.e.e],
+//         ]
+//     }
+// }
 
-#[repr(C)]
-#[derive(Debug, Default)]
-struct Output {
-    // TODO: move this out of here and make it a proper boxed model.
-    // Currently for simplicity, the state is just a u64.
-    next: u64,
-    display: DisplayData,
-}
+// #[repr(C)]
+// #[derive(Debug, Default)]
+// struct Output {
+//     // TODO: move this out of here and make it a proper boxed model.
+//     // Currently for simplicity, the state is just a u64.
+//     next: u64,
+//     display: DisplayData,
+// }
 
-fn roc_main(i: u64) -> Output {
-    #[link(name = "app")]
-    extern "C" {
-        #[link_name = "roc__mainForHost_1_exposed_generic"]
-        fn call(i: u64, out: &mut Output);
-    }
-    let mut out: Output = Default::default();
-    unsafe { call(i, &mut out) };
-    out
-}
+// fn roc_main(i: u64) -> Output {
+//     #[link(name = "app")]
+//     extern "C" {
+//         #[link_name = "roc__mainForHost_1_exposed_generic"]
+//         fn call(i: u64, out: &mut Output);
+//     }
+//     let mut out: Output = Default::default();
+//     unsafe { call(i, &mut out) };
+//     out
+// }
 
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
+#[embassy::main]
+async fn main(_spawner: Spawner, p: Peripherals) {
+    let mut col = Output::new(p.P0_28, Level::High, OutputDrive::Standard);
+    let mut row = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
 
-    let board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
-    let mut display = Display::new(board.display_pins);
-
-    let mut i = 0;
+    col.set_low();
     loop {
-        rprintln!("Sending state: {:?}", i);
-        let output = roc_main(i);
-        rprintln!("Roc generated: {:?}", output);
-        display.show(&mut timer, output.display.to_bytes(), 10);
-        i = output.next;
+        defmt::warn!("High");
+        row.set_high();
+        Timer::after(Duration::from_millis(1000)).await;
+        defmt::info!("Low");
+        row.set_low();
+        Timer::after(Duration::from_millis(1000)).await;
     }
 }
