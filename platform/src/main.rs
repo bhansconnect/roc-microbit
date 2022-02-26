a71513e9efd7a33b287899321a24d561efde641d
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-02-25 23:26:12 -0800

    re-enable calling roc app

--- platform/src/main.rs
+++ platform/src/main.rs
@@ -11,57 +11,57 @@ mod fmt;
 
 mod memory;
 
-// #[repr(C)]
-// #[derive(Debug, Default)]
-// struct Row {
-//     a: u8,
-//     b: u8,
-//     c: u8,
-//     d: u8,
-//     e: u8,
-// }
+#[repr(C)]
+#[derive(Debug, Default)]
+struct Row {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+}
 
-// #[repr(C)]
-// #[derive(Debug, Default)]
-// struct DisplayData {
-//     a: Row,
-//     b: Row,
-//     c: Row,
-//     d: Row,
-//     e: Row,
-// }
+#[repr(C)]
+#[derive(Debug, Default)]
+struct DisplayData {
+    a: Row,
+    b: Row,
+    c: Row,
+    d: Row,
+    e: Row,
+}
 
-// impl DisplayData {
-//     fn to_bytes(&self) -> [[u8; 5]; 5] {
-//         [
-//             [self.a.a, self.a.b, self.a.c, self.a.d, self.a.e],
-//             [self.b.a, self.b.b, self.b.c, self.b.d, self.b.e],
-//             [self.c.a, self.c.b, self.c.c, self.c.d, self.c.e],
-//             [self.d.a, self.d.b, self.d.c, self.d.d, self.d.e],
-//             [self.e.a, self.e.b, self.e.c, self.e.d, self.e.e],
-//         ]
-//     }
-// }
+impl DisplayData {
+    fn to_bytes(&self) -> [[u8; 5]; 5] {
+        [
+            [self.a.a, self.a.b, self.a.c, self.a.d, self.a.e],
+            [self.b.a, self.b.b, self.b.c, self.b.d, self.b.e],
+            [self.c.a, self.c.b, self.c.c, self.c.d, self.c.e],
+            [self.d.a, self.d.b, self.d.c, self.d.d, self.d.e],
+            [self.e.a, self.e.b, self.e.c, self.e.d, self.e.e],
+        ]
+    }
+}
 
-// #[repr(C)]
-// #[derive(Debug, Default)]
-// struct Output {
-//     // TODO: move this out of here and make it a proper boxed model.
-//     // Currently for simplicity, the state is just a u64.
-//     next: u64,
-//     display: DisplayData,
-// }
+#[repr(C)]
+#[derive(Debug, Default)]
+struct RocState {
+    // TODO: move this out of here and make it a proper boxed model.
+    // Currently for simplicity, the state is just a u64.
+    next: u64,
+    display: DisplayData,
+}
 
-// fn roc_main(i: u64) -> Output {
-//     #[link(name = "app")]
-//     extern "C" {
-//         #[link_name = "roc__mainForHost_1_exposed_generic"]
-//         fn call(i: u64, out: &mut Output);
-//     }
-//     let mut out: Output = Default::default();
-//     unsafe { call(i, &mut out) };
-//     out
-// }
+fn roc_main(i: u64) -> RocState {
+    #[link(name = "app")]
+    extern "C" {
+        #[link_name = "roc__mainForHost_1_exposed_generic"]
+        fn call(i: u64, out: &mut RocState);
+    }
+    let mut out: RocState = Default::default();
+    unsafe { call(i, &mut out) };
+    out
+}
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
@@ -69,12 +69,16 @@ async fn main(_spawner: Spawner, p: Peripherals) {
     let mut row = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
 
     col.set_low();
+    let mut state: RocState = Default::default();
     loop {
-        defmt::warn!("High");
-        row.set_high();
-        Timer::after(Duration::from_millis(1000)).await;
-        defmt::info!("Low");
-        row.set_low();
+        state = roc_main(state.next);
+        defmt::info!("Next State: {}", state.next);
+        defmt::debug!("{:?}", state.display.to_bytes());
+        if state.display.to_bytes()[4][3] == 0 {
+            row.set_low();
+        } else {
+            row.set_high();
+        }
         Timer::after(Duration::from_millis(1000)).await;
     }
 }
