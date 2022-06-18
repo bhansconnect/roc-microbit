bba732d457a471fa8fd9c16bf37c81dfa1bc8bd8
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-06-18 10:35:18 -0700

    git full display working

--- platform/src/main.rs
+++ platform/src/main.rs
@@ -4,8 +4,8 @@
 
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_nrf::Peripherals;
+use embassy_nrf::gpio::{AnyPin, Level, Output, OutputDrive, Pin};
+use embassy_nrf::{peripherals, Peripherals};
 
 mod fmt;
 
@@ -43,6 +43,66 @@ impl DisplayData {
     }
 }
 
+const DEFAULT_DELAY_MS: u64 = 2;
+struct Display<'d> {
+    cols: [Output<'d, AnyPin>; 5],
+    rows: [Output<'d, AnyPin>; 5],
+}
+
+impl<'d> Display<'d> {
+    fn new(
+        p0_28: peripherals::P0_28,
+        p0_11: peripherals::P0_11,
+        p0_31: peripherals::P0_31,
+        p1_05: peripherals::P1_05,
+        p0_30: peripherals::P0_30,
+        p0_21: peripherals::P0_21,
+        p0_22: peripherals::P0_22,
+        p0_15: peripherals::P0_15,
+        p0_24: peripherals::P0_24,
+        p0_19: peripherals::P0_19,
+    ) -> Display<'d> {
+        Display {
+            cols: [
+                Output::new(p0_28.degrade(), Level::High, OutputDrive::Standard),
+                Output::new(p0_11.degrade(), Level::High, OutputDrive::Standard),
+                Output::new(p0_31.degrade(), Level::High, OutputDrive::Standard),
+                Output::new(p1_05.degrade(), Level::High, OutputDrive::Standard),
+                Output::new(p0_30.degrade(), Level::High, OutputDrive::Standard),
+            ],
+            rows: [
+                Output::new(p0_21.degrade(), Level::Low, OutputDrive::Standard),
+                Output::new(p0_22.degrade(), Level::Low, OutputDrive::Standard),
+                Output::new(p0_15.degrade(), Level::Low, OutputDrive::Standard),
+                Output::new(p0_24.degrade(), Level::Low, OutputDrive::Standard),
+                Output::new(p0_19.degrade(), Level::Low, OutputDrive::Standard),
+            ],
+        }
+    }
+
+    async fn show(&mut self, data: &DisplayData, duration_ms: u64) {
+        let loops = duration_ms / (5 * DEFAULT_DELAY_MS);
+        let matrix = data.to_bytes();
+
+        for _ in 0..loops {
+            for (row, pixel_line) in self.rows.iter_mut().zip(matrix.iter()) {
+                row.set_high();
+                for (col, pixel) in self.cols.iter_mut().zip(pixel_line.iter()) {
+                    // TODO: deal with dimming.
+                    if *pixel > 0 {
+                        col.set_low();
+                    }
+                }
+                Timer::after(Duration::from_millis(DEFAULT_DELAY_MS)).await;
+                row.set_low();
+                for col in self.cols.iter_mut() {
+                    col.set_high();
+                }
+            }
+        }
+    }
+}
+
 #[repr(C)]
 #[derive(Debug, Default)]
 struct RocState {
@@ -65,20 +125,14 @@ fn roc_main(i: u64) -> RocState {
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
-    let mut col = Output::new(p.P0_28, Level::High, OutputDrive::Standard);
-    let mut row = Output::new(p.P0_21, Level::Low, OutputDrive::Standard);
-
-    col.set_low();
+    let mut disp = Display::new(
+        p.P0_28, p.P0_11, p.P0_31, p.P1_05, p.P0_30, p.P0_21, p.P0_22, p.P0_15, p.P0_24, p.P0_19,
+    );
     let mut state: RocState = Default::default();
     loop {
         state = roc_main(state.next);
         defmt::info!("Next State: {}", state.next);
         defmt::debug!("{:?}", state.display.to_bytes());
-        if state.display.to_bytes()[4][3] == 0 {
-            row.set_low();
-        } else {
-            row.set_high();
-        }
-        Timer::after(Duration::from_millis(1000)).await;
+        disp.show(&state.display, 1000).await;
     }
 }
