4fc7d36c0f4a9a7a8daf75f00a355bf3390a213a
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-06-26 18:41:08 -0700

    fix format timestamp to static size

--- platform/src/fmt.rs
+++ platform/src/fmt.rs
@@ -5,7 +5,7 @@ use panic_probe as _;
 
 use core::sync::atomic::{AtomicUsize, Ordering};
 
-defmt::timestamp! {"{=u64}", {
+defmt::timestamp! {"{=u64:08}", {
         static COUNT: AtomicUsize = AtomicUsize::new(0);
         // NOTE(no-CAS) `timestamps` runs with interrupts disabled
         let n = COUNT.load(Ordering::Relaxed);
